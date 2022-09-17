41bdddcd7f09d2ca70881c76668b34f959a26b03
Serban Ciofu <sciofu@gmail.com>
2022-09-17 10:55:38 +0300

    hello message for example

--- main/Main.java
+++ main/Main.java
@@ -18,6 +18,8 @@ final class Main {
 
         Map map = new Map(n, m);
 
+        System.out.println("Hello" + map);
+
         for (i = 0; i < n; ++i) {
             c = file.nextWord();
             for (j = 0; j < m; ++j) {
